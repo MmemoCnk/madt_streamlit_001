4d27b2543ff49f6301b0cb6d12bbeadb2cfccca3
MmemoCnk <52519954+MmemoCnk@users.noreply.github.com>
2025-01-26 07:24:36 +0000

    add new filter

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import random
- 
+import datetime 
+
+st.set_page_config(layout='wide')
 st.title('Test Streamlit')
-# result1 = st.button('click me!')
-# if result1:
-#     st.write('you click 1')
 
-# result2 = st.button('click me2!',type='tertiary')
-# if result1 & result2:
-#     st.write('you click both')
 
-st.write('Hello World!')
+result1 = st.button('click me1!')
+if result1:
+    st.write('you click on 1')
+
+result2 = st.button('click me2!',type='tertiary')
+if result1 and result2:
+    st.write('you click both')
+
+st.button("Reset",type='primary')
+
+# st.write('Hello World!')
  
-if st.button('Generate Random Number'):
-    random_number = random.randint(1, 100)
-    st.write(f'Random Number: {random_number}')
+# if st.button('Generate Random Number'):
+#     random_number = random.randint(1, 100)
+#     st.write(f'Random Number: {random_number}')
