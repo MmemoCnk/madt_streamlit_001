e5199f488d942aa51296480b28462684b7ada22a
MmemoCnk <52519954+MmemoCnk@users.noreply.github.com>
2025-01-26 07:16:56 +0000

    test commit

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import streamlit as st
 import random
  
 st.title('Test Streamlit')
+# result1 = st.button('click me!')
+# if result1:
+#     st.write('you click 1')
+
+# result2 = st.button('click me2!',type='tertiary')
+# if result1 & result2:
+#     st.write('you click both')
+
 st.write('Hello World!')
  
 if st.button('Generate Random Number'):
